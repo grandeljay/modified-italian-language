2a674eda985b70e8be55cc372bf85bc32eaac58d
grandeljay <github.jay@grandel.anonaddy.me>
2023-02-23 09:30:07 +0100

    refactor: use stdmodule constructor

--- src/admin/includes/modules/system/grandeljay_italian_language.php
+++ src/admin/includes/modules/system/grandeljay_italian_language.php
@@ -19,11 +19,7 @@ class grandeljay_italian_language extends StdModule
 
     public function __construct()
     {
-        if (function_exists('parent::__construct()')) {
-            parent::__construct();
-        } else {
-            $this->init('MODULE_' . strtoupper(self::class));
-        }
+        parent::__construct();
 
         $this->checkForUpdate(true);
     }
